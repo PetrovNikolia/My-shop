289f418f699f4960aa440c46e507092edeb66b0e
Николая <Kintep84@mail.ru>
2020-12-06 15:13:40 +0500

    Dz6

---
+++ rabbit-mq-sender/src/test/java/ru/petrov/RabbitMqSenderApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.petrov;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RabbitMqSenderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
